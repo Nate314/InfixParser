d3c0111b138186c8a7edc1576fee943677d897ec
Nate314 <NathanGawith@gmail.com>
2018-09-20 21:14:06 -0500

    Added definition to eval function
    eval returns an int for the passed string. It basically determines which function (evalPostMath or evalPostBool) to route the equation to.

--- Evaluator.cpp
+++ Evaluator.cpp
@@ -1,12 +1,61 @@
 #include "Evaluator.h"
+#include <stack>
 
 Evaluator::Evaluator()
 {
 }
 
+bool Evaluator::isBooleanEquation(string equation) {
+	for (std::map<string, int>::iterator iter = booleanOperators.begin(); iter != booleanOperators.end(); iter++)
+		if (equation.find(iter->first) != -1) return true;
+	return false;
+}
+
 int Evaluator::eval(string equation)
 {
-	return 0;
+	// resulting int to return
+	int result = 0;
+	// check if the equation passed has any boolean operators
+	bool isEquationABooleanEquation = isBooleanEquation(equation);
+	// if the equation passed has boolean operators,
+	//   evaluate each math portion and build string for boolean equation
+	if (isEquationABooleanEquation) {
+		// boolean equation that will be built
+		string booleanEquation = "";
+		bool hasBooleanOperators = true;
+		// while the equation contains boolean operators
+		while (hasBooleanOperators) {
+			bool foundBooleanOperator = false;
+			// search for a boolean operator
+			for (std::map<string, int>::iterator iter = booleanOperators.begin(); iter != booleanOperators.end(); iter++) {
+				// if this is the first boolean operator found
+				if (!foundBooleanOperator && equation.find(iter->first) != -1) {
+					foundBooleanOperator = true;
+					// add math portion before the boolean operator to the booleanEquation string
+					booleanEquation += evalPostMath(equation.substr(0, equation.find(iter->first)));
+					// remove the evaluated portion from the equation
+					equation = equation.substr(equation.find(iter->first) + iter->first.length, equation.length - 1);
+					// add the boolean operator to the booleanEquation string
+					booleanEquation += equation.substr(0, iter->first.length);
+					// remove the boolean operator from the equation
+					equation = equation.substr(iter->first.length, equation.length - 1);
+				}
+			}
+			// re-evaluate if the expression still has a boolean operator
+			hasBooleanOperators = isBooleanEquation(equation);
+		}
+		// evaluate the final part of the equation and add it to the booleanEquation string
+		booleanEquation += evalPostMath(equation);
+		// return the result of the boolean equation
+		return evalPostBool(booleanEquation);
+	}
+	// if the equation passed does not have boolean operators,
+	//   evaluate math result
+	else {
+		result = evalPostMath(equation);
+	}
+	// return result
+	return result;
 }
 
 // Nathan
--- Evaluator.h
+++ Evaluator.h
@@ -1,14 +1,17 @@
 #include <string>
 #include <iostream>
+#include <map>
 using namespace std;
 #pragma once
 
 class Evaluator {
 private:
+	std::map<string, int> booleanOperators = { {"!", 8}, {">", 4}, {">=", 4}, {"<", 4}, {"<=", 4}, {"==", 3}, {"!=", 3}, {"&&", 2}, {"||", 1} };
 	bool evalPostBool(string equation);
 	int evalPostMath(string equation);
 	string toPostMath(string equation);
 	string toPostBool(string equation);
+	bool isBooleanEquation(string equation);
 public:
 	Evaluator();
 	int eval(string equation);
